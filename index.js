fd4faf83f20fc925e58328e346c73f0554956060
Ryan Harris <RyanHarr33@gmail.com>
2021-08-24 19:20:57 -0500

    working backwards to render page

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ import {capitalize} from "lodash";
 import axios from "axios";
 
 const router = new Navigo(window.location.origin);
-// const quizContainer = document.getElementById('quiz');
-// const resultsContainer = document.getElementById('results');
-// const submitButton = document.getElementById('submit');
-// Get the navbar
 const navbar = document.getElementById("navbar");
 // Get the offset position of the navbar
 // const sticky = navbar.offsetTop;
@@ -27,7 +23,7 @@ function render(st = state.Home) {
   `;
   router.updatePageLinks();
   addEventListeners(st);
-}
+};
 
 function addEventListeners(st) {
 // add event listeners to Nav items for navigation
@@ -51,27 +47,27 @@ navLink.addEventListener("click", (event) => {
 //   });
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {
-  myFunction();
-};
+// window.onscroll = function() {
+//   myFunction();
+// };
 
-router.hooks({
-  before: (done, params) => {
-    const page =
-      params && params.hasOwnProperty("page")
-        ? capitalize(params.page)
-        : "Home";
-  }
-});
+// router.hooks({
+//   before: (done, params) => {
+//     const page =
+//       params && params.hasOwnProperty("page")
+//         ? capitalize(params.page)
+//         : "Home";
+//   }
+// });
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// };
 
 router.on({
   ":page" : (params) => render(state[capitalize(params.page)]),
