5d13dc52dbe9ea1b2cde9d4d602aa8ec0779f6b7
Ryan Harris <RyanHarr33@gmail.com>
2021-09-02 15:27:57 -0500

    retyped about us export

--- store/index.js
+++ store/index.js
@@ -1,7 +1,7 @@
 export { default as Home } from "./Home";
 export { default as Quiz } from "./Quiz";
 export { default as Doginfo } from "./Doginfo";
-export { default as Aboutus } from "./Aboutus";
 export { default as Links } from "./Links";
 export { default as Veterinarians } from "./Veterinarians";
 export { default as Quizresult } from "./Quizresult";
+export { default as Aboutus } from "./Aboutus";
