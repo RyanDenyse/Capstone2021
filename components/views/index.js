26b1c5f9676caa1174edb8c736025c726e2c3487
Ryan Harris <RyanHarr33@gmail.com>
2021-09-02 15:32:42 -0500

    removed about us from views

--- components/views/index.js
+++ components/views/index.js
@@ -1,6 +1,6 @@
 export { default as Home } from "./Home";
 export { default as Doginfo } from "./Doginfo";
 export { default as Quiz } from "./Quiz";
-export { default as Aboutus } from "./Aboutus";
+// export { default as Aboutus } from "./Aboutus";
 export { default as Veterinarians } from "./Veterinarians";
 export { default as Quizresult } from "./Quizresult";
